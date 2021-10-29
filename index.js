a1959c4b041bf0d4419266e58cdd99ac35dcc93e
javaid05 <javaidmalik.khan@gmail.com>
2021-10-29 13:23:03 +0100

    Q1 completed

---
+++ index.js
@@ -0,0 +1,18 @@
+/*
+ASSIGNMENT RULES
+- All the answers must be written in JavaScript
+- You can ask for help, reach the Teaching Assistants if needed
+- You can Google / use StackOverflow BUT only when you think you need something we didn't cover during lessons yet
+- You can test your code in a separate file or de-commenting the single exercises in this one.
+- You can use the bash terminal, the VSCode terminal or the one embedded in your Operating System if you're using macOS or Linux.
+- The solution must be pushed to the repository and be available for the tutors by the end of the day (5PM CET)
+*/
+
+/* EXERCISE 1
+ Write a function called "area" which receives 2 parameters (l1, l2) and calculates the area of the associated rectangle.
+*/
+const area = function (l1, l2) {
+  let traianglArea = (l1 * l2) / 2;
+  console.log("Area of triaangle:", traianglArea);
+};
+area(6, 6);
